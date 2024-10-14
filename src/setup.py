c4373ffe044f1185ac4e4e99d55f691f5a5ab828
ursister <106648827+ursisterbtw@users.noreply.github.com>
2024-10-14 01:22:48 -0400

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -3,10 +3,8 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
-# Get the absolute path of the current file
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Construct the path to main.pyx
 main_pyx_path = os.path.join(current_dir, "main.pyx")
 
 extensions = [
